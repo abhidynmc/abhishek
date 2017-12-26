1338916e3afb102c95088a78dac0b5b14d7a6bbb
Abhishek <abhi_2611@hotmail.com>
2017-12-26 22:56:00 +0530

    Commit 4

---
+++ NodeJS/contactlist/routes/route.js
@@ -0,0 +1,68 @@
+const express= require("express");
+const router= express.Router();
+
+const Contact= require('../models/contacts');
+
+//retrieving data
+router.get('/contacts', (req, res, next)=>{
+    Contact.find(function(err, contacts){
+        res.json(contacts);
+    });
+});
+
+//add contact
+router.post('/contact', (req, res, next)=>{
+    //logic to add contact
+    let newContact= new Contact({
+        first_name:req.body.first_name, 
+        last_name:req.body.last_name,
+        phone:req.body.phone
+    });
+
+    newContact.save((err, contact)=>{
+        if(err){
+            res.json({msg: 'Failed to add contacts'});
+        }
+        else{
+            res.json({msg: 'Contacts added successfully' });
+        }
+    });
+    
+
+});
+router.post('/contact/update/:id', (req, res, next)=>{
+    //logic to update
+    var id=req.params.id;
+    let newContact= new Contact({
+        _id:req.params.id,
+        first_name:req.body.first_name, 
+        last_name:req.body.last_name,
+        phone:req.body.phone
+    });
+
+Contact.findByIdAndUpdate(id, newContact, {new :true}, function(err, result){
+    if(err){
+        res.json({msg: 'Failed to update contact'});
+    }
+    else{
+        // res.json({msg: 'Contact ' +newContact.first_name+' '+newContact.last_name+ ' updated successfully'});
+        res.json(result);
+    }
+
+});
+});
+
+//delete contact
+router.delete('/contact/:id', (req, res, next)=>{
+    //logic to delete contact
+    Contact.remove({_id: req.params.id}, function(err, result){
+        if(err){
+            res.json(err);
+        }
+        else{
+            res.json(result);
+        }
+    });
+});
+
+module.exports= router; 
