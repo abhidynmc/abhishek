b29f45ddf45079fa3ddb3d1808097b92e3bc3c09
Abhishek Saxena <abhi_2611@hotmail.com>
2018-08-10 02:03:45 +0530

    Made the navbar

--- NodeJS/ManageAll/index.js
+++ NodeJS/ManageAll/index.js
@@ -8,6 +8,5 @@ http.createServer(function (request, response) {
     
     // Send the response body as "Hello World"
     response.end('Hello World\n');
- }).listen(8081);
- 
+ }).listen(8081); 
  // Console will print the message
